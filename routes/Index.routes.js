ad7bf9f9651e476af52ddae1def1d24e53b438fb
captcha781 <bhuvanesh19112001@gmail.com>
2023-04-07 19:01:15 +0530

    Blurview fixes

--- routes/Index.routes.js
+++ routes/Index.routes.js
@@ -1,29 +1,63 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-import Banner from '../screens/Banner'
-import Signin from '../screens/Signin'
-import Signup from '../screens/Signup'
+import Banner from "../screens/Banner";
+import Signin from "../screens/Signin";
+import Signup from "../screens/Signup";
 import Home from "../screens/Home";
 import TabsNavigation from "./Tab.navigator";
 import DrawerNavigation from "./Drawer.navigator";
 
+const config = {
+  animation: "spring",
+  config: {
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+    bounciness: 0.5,
+    speed: 0.5
+  },
+};
 
 const screens = {
-    Banner: {
-        screen: Banner,
+  Banner: {
+    screen: Banner,
+    navigationOptions: {
+      transitionSpec: {
+        open: config,
+        close: config,
+      },
     },
-    Signin: {
-        screen: Signin,
+  },
+  Signin: {
+    screen: Signin,
+    navigationOptions: {
+      transitionSpec: {
+        open: config,
+        close: config,
+      },
     },
-    Signup: {
-        screen: Signup,
+  },
+  Signup: {
+    screen: Signup,
+    navigationOptions: {
+      transitionSpec: {
+        open: config,
+        close: config,
+      },
     },
-    Home: {
-        screen: DrawerNavigation
-    }
-}
+  },
+  Home: {
+    screen: DrawerNavigation,
+    navigationOptions: {
+      transitionSpec: {
+        open: config,
+        close: config,
+      },
+    },
+  },
+};
 
-const IndexStack = createStackNavigator(screens, { headerMode: 'none' })
+const IndexStack = createStackNavigator(screens, { headerMode: "none" });
 
-export default createAppContainer(IndexStack)
+export default createAppContainer(IndexStack);
--- screens/Signin.js
+++ screens/Signin.js
@@ -1,12 +1,68 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, ImageBackground, Pressable, KeyboardAvoidingView } from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-gesture-handler";
+import { BlurView } from "@react-native-community/blur";
+import SafeAreaView from "react-native-safe-area-view";
 
 const Signin = ({ navigation }) => {
-  return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Button title='Home ?' onPress={() => navigation.navigate('Home')} />
-    </View>
-  )
-}
+  const [btns, setBtns] = useState({ submit: false });
 
-export default Signin
+  const effectToggle = (id) => {
+    return setBtns({ ...btns, [id]: !btns[id] });
+  };
+
+  return (
+    <View className="flex-1 justify-center items-center bg-purple-400">
+      <ImageBackground
+        className="flex-1 w-full justify-center"
+        source={require("../assets/banner/background.jpg")}
+      >
+        <View className="mb-5 w-11/12 mx-auto">
+          <Text className="text-white font-semibold font-lexend text-2xl">
+            Signin
+          </Text>
+        </View>
+        <View className="mt-0 w-11/12 mx-auto">
+          <View className="w-full mb-3">
+            <Text className="text-white font-lexend mb-3">Username</Text>
+            <View className="rounded-md overflow-hidden relative">
+              <TextInput
+                className="w-full rounded-md p-2 text-white font-lexend"
+                style={{ backgroundColor: "rgba(255,255,255,0.35)" }}
+              />
+            </View>
+          </View>
+          <View className="w-full mb-8">
+            <Text className="text-white font-lexend mb-3">Password</Text>
+            <View className="rounded-md overflow-hidden">
+              <View blurType="light" blurAmount={10}>
+                <TextInput
+                  className="w-full rounded-md p-2 text-white font-lexend"
+                  style={{ backgroundColor: "rgba(255,255,255,0.35)" }}
+                  secureTextEntry
+                />
+              </View>
+            </View>
+          </View>
+          <View className="w-full mb-3 overflow-hidden rounded-md">
+            <Pressable
+              className="bg-purple-500 rounded-md py-3 border-[1px] border-purple-500 active:bg-transparent"
+              onPressIn={() => effectToggle("submit")}
+              onPressOut={() => effectToggle("submit")}
+            >
+              <Text
+                className={`text-center font-lexend ${
+                  btns.submit ? "text-white" : "text-white"
+                }`}
+              >
+                Sign in
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Signin;
